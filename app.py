e277453d9e28890de3c43f91e315292a11084a87
orios-github <135855283+orios-github@users.noreply.github.com>
2026-01-14 23:45:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return "Hello DevOps, this has been a long journey!, let's try a newer version: v25."
+    return "Hello DevOps, this has been a long journey!, let's try a newer version: v26."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
